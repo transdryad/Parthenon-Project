4c8e4105a56dce5c67c1f7947878893c7267e13d
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-11-15 09:57:34 -0500

    reorganize

--- src/server.cpp
+++ src/server.cpp
@@ -4,21 +4,13 @@
 #include "src/question.hpp"
 #include "src/toml.hpp"
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "A question file is required" << std::endl;
-        return 1;
-    }
-
+std::vector<Question> parse(std::string filename) {
     toml::table tbl;
     std::vector<Question> questions;
 
-    try //parse toml into memory
-    {
-        tbl = toml::parse_file(argv[1]);
-    }
-    catch (const toml::parse_error& err)
-    {
+    try { //parse toml into memory
+        tbl = toml::parse_file(filename);
+    } catch (const toml::parse_error& err) {
         std::cerr << "Parsing failed:\n" << err << "\n";
         return 1;
     }
@@ -27,14 +19,26 @@ int main(int argc, char* argv[]) {
     for (size_t i = 0; i < tbl["questions"].as_array()->size(); i++) { //put questions in our nice data structure
         std::optional<std::string> question = tbl["questions"][i][0].value<std::string>();
         std::optional<int> correct = tbl["questions"][i][2].value<int>();
+
         std::string answers[4];
         for (int j = 0; j < 4; j++) {
             std::optional<std::string> thing = tbl["questions"][i][1][j].value<std::string>();
             answers[j] = thing.value_or("");
         }
-        
+
         questions.emplace_back(question.value_or(""), answers, correct.value_or(0));
     }
+
+    return questions;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "A question file is required" << std::endl;
+        return 1;
+    }
+
+    std::vector<Question> questions = parse(argv[1]);
 
     for (auto q : questions) {
         std::cout << q.question << ":";
